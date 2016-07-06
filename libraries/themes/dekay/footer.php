2cfdf0cf2ddb4827dd448fb6528700f1983f370b
rparks225 <sandrockc225@aol.com>
2016-07-05 22:18:17 -0400

    musicTheme

---
+++ libraries/themes/dekay/footer.php
@@ -0,0 +1,42 @@
+</div>
+</div>
+<!--Close Cont-->
+
+
+<!--Open Footer-->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="title">
+                    <h5>About Dekay</h5>
+                    <hr> </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <div class="clearfix"></div>
+            </div>
+            <div class="col-md-4">
+                <div class="title">
+                    <h5>Quick Links</h5>
+                    <hr> </div>
+                <ul class="col-md-6 col-sm-6 col-xs-6">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Music</a></li>
+                    <li><a href="#">Videos</a></li>
+                </ul>
+                <ul class="col-md-6 col-sm-6 col-xs-6">
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">News/Media</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <div class="col-md-4">
+                <div class="title">
+                    <h5>Copyright</h5>
+                    <hr> </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <br> <a href="#">Admin Login.</a> </div>
+        </div>
+    </div>
+</footer>
+<!--Close Footer-->
---
+++ libraries/themes/dekay/html_blocks/metaScript.php
@@ -0,0 +1,54 @@
+
+<?php
+
+$url = new cms();
+$url->parseUrl();
+$tits = array($url->parseUrl());
+
+$siteN = "themeParks";
+
+$metaKey = array(
+
+''.title().''=>array(
+
+'name'=>''.$title.'',
+
+'title'=>'
+<title>'.$siteN.' - '.$heading.'</title>',
+
+'keyWords'=>'<meta name="keywords" content="'.$keywords.'" />',
+
+'script'=>'
+<meta name="description" content="'.$script.'" />',
+
+'link'=>'
+<link rel="canonical" href="'.links().'">'
+	
+)
+
+);
+
+if($script == 'false'){
+	
+	echo '<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
+	echo $metaKey[title()]['title'];
+	
+	}else if(isset($tits[2])){
+			
+			echo '<meta name="keywords" content="Posts, Blog posts" />
+<meta name="description" content="Posts for all things posts! '.bloggy($tits[2]).'" />
+<title>'.$siteN.' - News Post - '.bloggy($tits[2]).'</title>
+';
+			
+			}else if(title() === $metaKey[title()]['name']){
+		
+		echo $metaKey[title()]['keyWords'];
+		echo $metaKey[title()]['script'];
+		echo $metaKey[title()]['link'];
+		echo $metaKey[title()]['title'];
+		
+		};		
+			
+?>
+
+        
