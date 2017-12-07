54fe26fdf4bcda859a06d10370ced28f8bb8c041
rparks225 <sandrockc225@aol.com>
2017-12-07 13:35:15 -0500

    New Temaplate & SQL Compat.
    • Fixed Compatibility issue between MYSQL and MariaDB.
    • Finished out MDBootstrap template.

--- libraries/themes/mdBootstrap/views/contact/contact_view.php
+++ libraries/themes/mdBootstrap/views/contact/contact_view.php
@@ -25,41 +25,47 @@
             <form method="post" id="schedule" action="">
 
                 <input type="hidden" name="op" value="" />
+         
+                   <div class="row my-5">
+                      <div class="col-md-6">
+                          <div class="md-form ">                    
+                              <input name="name" required type="text" class="form-control validate" id="name">
+                              <label for="name">Name</label>
+                          </div>     
+                       </div>
 
-               <div class="col-md-6 pull-left">
-                   <div class="md-form">                    
-                       <input name="name" type="text" required class="form-control" id="name">
-                       <label for="name">Name</label>
+                       <div class="col-md-6">
+                           <div class="md-form ">                
+                               <input name="email" required type="email" class="form-control validate" id="email">
+                               <label for="email">Email</label>
+                           </div>  
+                       </div>
                    </div>
-                </div>                
+                              
+                    <div class="row my-5">
+                       <div class="col-md-6">
+                           <div class="md-form ">                   
+                               <input name="address" required type="text" class="form-control validate" id="address">
+                               <label for="address">Address</label>
+                           </div>       
+                        </div>
 
-                <div class="col-md-6 pull-left">
-                    <div class="md-form">                
-                       <input name="email" type="email" required class="form-control" id="email" />
-                       <label for="email">Email</label>
-                   </div>
-                </div>                
-
-                <div class="col-md-6 pull-left">
-                    <div class="md-form">                   
-                        <input name="address" type="text" required class="form-control" id="address" />
-                       <label for="address">Address</label>
-                   </div>
-                </div>                
-
-               <div class="col-md-6 pull-left">
-                   <div class="md-form">                    
-                    <input name="zip" type="text" required class="form-control" id="zip" />
-                    <label for="zip">Zip</label>
-                </div>
-               </div>                
-
-               <div class="col-md-12 pull-left">
-                   <div class="md-form">                    
-                    <textarea type="text" name="comment" class="md-textarea" id="comment"></textarea>
-                    <label for="comment">Comment</label>
-                </div>
-               </div>                
+                        <div class="col-md-6">
+                            <div class="md-form ">                    
+                                <input name="zip" required type="text" class="form-control validate" id="zip">
+                                <label for="zip">Zip</label>
+                            </div>            
+                        </div>
+                    </div>          
+                  
+                   <div class="row my-5">
+                      <div class="col-md-12">
+                          <div class="md-form">                    
+                              <textarea type="text" name="comment" class="form-control md-textarea" id="comment"></textarea>
+                              <label for="comment" class="">Comment</label>
+                          </div>      
+                      </div>
+                   </div>                          
 
                 <div class="clearfix"></div> 
 
