e60716d0e7f9ed917cfcd11c85964858238189e0
rparks225 <sandrockc225@aol.com>
2017-12-09 12:42:54 -0500

    Dashboard Optimization - Romoved Precompiles CSS/js
    • Fixed mobile version of JS compiler
    • Got rid of pre compiled sheets

--- includes/admin/dash_view.php
+++ includes/admin/dash_view.php
@@ -150,7 +150,7 @@ $insert->uped($location,$query,$method);
         </div>
     </div>
    
-    <div class="col s2 m4">
+    <div class="col s12 m4">
         <div class="card blue-grey darken-1">
             <div class="card-content white-text">
                 <span class="card-title">JS Compiler</span>
