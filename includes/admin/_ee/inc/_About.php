d92ac69f9e8e8625f3b413c26986cd612663062f
rparks225 <sandrockc225@aol.com>
2018-01-11 11:23:49 -0500

    Updated Grid system for testing
    • Title explains it all

--- includes/admin/_ee/inc/_About.php
+++ includes/admin/_ee/inc/_About.php
@@ -1,16 +1,18 @@
+<div class="container">
 <div class="btgrid">
 <div class="row row-1">
-<div class="col col-md-6">
+<div class="col-md-6">
 <div class="content">
 <p><img class="img-fluid lazy" data-original="https://unsplash.it/500/500/?random" src="https://unsplash.it/500/500/?random" style="width: 100%;" /></p>
 </div>
 </div>
 
-<div class="col col-md-6">
+<div class="col-md-6">
 <div class="content">
 <h3>Edit this shit</h3>
 
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita consectetur consequatur, modi aliquid laudantium corporis asperiores laboriosam est! Ipsam dolorum nobis soluta, sint, aut velit magnam maxime consequuntur quasi expedita sed esse maiores optio laborum, vero assumenda, ut rerum libero.</p>
+</div>
 </div>
 </div>
 </div>
