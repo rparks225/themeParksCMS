236a5d6ba4626461984953f02f1c94693f73b00c
rparks225 <sandrockc225@aol.com>
2018-09-11 11:25:35 -0400

    Updated Admin Model
    • Updated Admin Model so thats its easier to add Vars as needed.

--- apps/cms/model/admin_model.php
+++ apps/cms/model/admin_model.php
@@ -31,47 +31,55 @@ class admin_model{
             
             while($row = $result->fetch_assoc()){
 
-                $loops = array(
-                    'Id'=>'id',
-                    'Title'=>'title',
-                    'Html'=>'html',
-                    'Description'=>'script',
-                    'Links'=>'links',
-                    'Image'=>'img',
-                    'Alt'=>'alt',
-                    'Price'=>'price',
-                    'Gallery'=>'album',
-                    'Links'=>'link',
-                    'First'=>'first',
-                    'Last'=>'last',
-                    'Email'=>'email',
-                    'Phone'=>'phone',
-                    'State'=>'state',
-                    'Message'=>'message',
-                    'Date'=>'date',
-                    'Heading'=>'heading',
-                    'KeyWords'=>'keywords',
-                    'Editable'=>'edit',
-                    'Job'=>'job',
-                    'Facility'=>'facility',
-                    'Updated'=>'update',
-                    'Cover'=>'cover',
-                    'Album'=>'gallery',
-                    'fourth'=>'fourth',
-                    'Badges'=>'badges',
-                    'Company'=>'company',
-                    'Grade'=>'grade',
-                    'Caption'=>'imgCap',
-                    'User_name'=>'users',
-                    'Display_name'=>'displName',
-                    'Pass_word'=>'pass',
-                    'Signed_Up'=>'signedUp',
-                    'Privileges'=>'privi',
-                    'Menu_Name'=>'menNam',
-                    'Nav_Text'=>'navTxt',
-                    'Order'=>'order',
-                    'COUNT(Id)'=>'count'
-                );
+				$loops = array(
+					//Page & Post Global Vars
+					'Id'=>'id',
+					'Title'=>'title',
+					'Html'=>'html',
+					'Description'=>'script',
+					'Links'=>'links',
+					'Image'=>'img',
+					'Alt'=>'alt',
+					'KeyWords'=>'keywords',
+					'Editable'=>'edit',
+					'Links'=>'link',
+					'Heading'=>'heading',
+					'Caption'=>'imgCap',
+
+					//Gallery Vars
+					'Gallery'=>'album',
+					'Cover'=>'cover',
+					'Album'=>'gallery',
+
+					//Email Vars
+					'First'=>'first',
+					'Last'=>'last',
+					'Email'=>'email',
+					'Phone'=>'phone',
+					'State'=>'state',
+					'Message'=>'message',
+					'Date'=>'date',
+
+					//Job Vars
+					'Job'=>'job',
+					'Facility'=>'facility',
+					'Updated'=>'update',
+					'fourth'=>'fourth',
+
+					//User Vars
+					'User_name'=>'users',
+					'Display_name'=>'displName',
+					'Pass_word'=>'pass',
+					'Signed_Up'=>'signedUp',
+					'Privileges'=>'privi',
+					'User_Img'=>'useImg',
+
+					//Back End Nav Vars
+					'Menu_Name'=>'menNam',
+					'Nav_Text'=>'navTxt',
+					'Order'=>'order',
+					'COUNT(Id)'=>'count'
+				);
 
                 foreach($loops as $loop => $val){
 
